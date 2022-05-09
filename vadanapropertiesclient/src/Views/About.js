df43ffbcce5a6f010578eedd1e8d3040a8378133
david vareba <davidvareba@gmail.com>
2022-05-09 16:05:53 -0500

    added the Home and About views

---
+++ vadanapropertiesclient/src/Views/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+    return(
+<div class="card mb-3">
+  <img src="Images/Logo.jpg" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h2 class="card-title">Vadana Properties</h2>
+    <p class="card-text">Real Estate Company Based out of Nashville, Tennessee. We offer the most reliable real estate services in Middle Tennesse. Let us take you home!</p>
+  </div>
+</div>
+    )
+}
---
+++ vadanapropertiesclient/src/Views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <><div>
+      </div><div>
+              <h1 className="text-center">Vadana Properties</h1>
+              <img src="Images/Logo.jpg" class="card-img-top" alt="..."></img>
+          </div></>
+  );
+}
